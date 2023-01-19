981511c82efad57e1145fe39908b5b443d6b5343
git_DavidQiuMAC <yangqiu2024@u.northwestern.edu>
2023-01-18 21:06:40 -0600

    warmup2

--- src/crypto/merkle.rs
+++ src/crypto/merkle.rs
@@ -1,29 +1,109 @@
 use super::hash::{Hashable, H256};
-
+use std::collections::VecDeque;
+use ring::digest;
 /// A Merkle tree.
 #[derive(Debug, Default)]
 pub struct MerkleTree {
+    level:usize,
+    tree_node: H256,
 }
 
 impl MerkleTree {
     pub fn new<T>(data: &[T]) -> Self where T: Hashable, {
-        unimplemented!()
+        //unimplemented!()
+        let mut num_data = data.len();
+        let mut num_temp=0;
+        let mut tree_node: Vec<H256> = Vec::new();
+        let mut tree_queue: VecDeque<H256> = VecDeque::new();
+
+        if num_data==0{
+            let scope: [u8; 32] = [0; 32];
+            tree_node.push(scope.into());
+
+            return MerkleTree{level:0, tree_node:tree_node};
+        }
+
+        if num_data % 2 != 0 {
+            tree_queue.push_back(data[num_data - 1].hash());
+            num_data= num_data+1;
+        }
+
+        for t in data.into_iter() {
+            tree_queue.push_back(t.hash());
+        }
+
+        let mut num_data_temp = num_data;
+
+        while !tree_queue.is_empty() {
+            let mut tree_hash = digest::Context::new(&digest::SHA256);
+
+            let left_child = tree_queue.pop_front().unwrap();
+            tree_node.push(left_child);
+            num_temp = num_temp + 1;
+            tree_hash.update(left_child.as_ref());
+
+            let node_s = tree_queue.pop_front();
+            if node_s == None {break; }
+
+            let right_child = node_s.unwrap();
+            tree_node.push(right_child);
+            num_temp = num_temp + 1;
+            tree_hash.update(right_child.as_ref());
+
+            let node_parent: H256 = tree_hash.finish().into();
+            tree_queue.push_back(node_parent);
+
+            if num_temp != 2  && num_temp == num_data_temp {
+                num_temp = 0;
+                if (num_data_temp / 2) % 2 == 0 {
+                    num_data_temp = num_data_temp / 2;
+                } 
+                else {
+                    tree_queue.push_back(node_parent);
+                    num_data_temp = num_data_temp / 2 + 1;
+                }
+            }
+        }
+
+        MerkleTree {
+            level: num_data.
+            tree_node: tree_node,
+        }
     }
 
     pub fn root(&self) -> H256 {
-        unimplemented!()
+        //unimplemented!()
+        self.tree_node[self.tree_node.len() - 1]
+
     }
 
     /// Returns the Merkle Proof of data at index i
     pub fn proof(&self, index: usize) -> Vec<H256> {
-        unimplemented!()
+        //unimplemented!()
     }
 }
 
 /// Verify that the datum hash with a vector of proofs will produce the Merkle root. Also need the
 /// index of datum and `leaf_size`, the total number of leaves.
 pub fn verify(root: &H256, datum: &H256, proof: &[H256], index: usize, leaf_size: usize) -> bool {
-    unimplemented!()
+   // unimplemented!()
+   let mut idx = index;
+   let mut node = *datum;
+   let mut temp = digest::Context::new(&digest::SHA256);
+
+   for t in 0..(proof.len()) {
+    if idx % 2 != 0 {
+        temp.update(proof[t].as_ref());
+        temp.update(node.as_ref());
+    } 
+    if idx % 2 == 0{
+        temp.update(node.as_ref());
+        temp.update(proof[t].as_ref());
+    }
+    idx = idx / 2;
+    node = temp.finish().into();
+}
+return node == *root;
 }
 
 #[cfg(test)]
--- src/transaction.rs
+++ src/transaction.rs
@@ -1,18 +1,54 @@
 use serde::{Serialize,Deserialize};
-use ring::signature::{Ed25519KeyPair, Signature, KeyPair, VerificationAlgorithm, EdDSAParameters};
+use ring::signature::{self,Ed25519KeyPair, Signature, KeyPair, VerificationAlgorithm, EdDSAParameters};
+use rand::Rng;
+use ring::digest;
+use super::hash::{Hashable, H256};
+use crate::crypto::hash::tests::generate_random_hash;
 
+
+//[Jan.15, 2023 for Warmup2]
 #[derive(Serialize, Deserialize, Debug, Default)]
 pub struct Transaction {
+    pub input_data: Vec<input_mode>,
+    pub output_data: Vec<output_mode>
+}
+#[derive(Serialize, Deserialize, Debug, Default)]
+pub struct input_mode{
+    pub message: H256,
+    //println!("input data success!")
 }
 
+#[derive(Serialize, Deserialize, Debug, Default)]
+pub struct output_mode{
+    pub message: H256,
+    //println!("input data su12ccess!")
+}
+// #[derive(Serialize,Deserialize,Debug,Default)]
+// pub struct signed_Transaction{
+// }
+impl Hashable for Transaction {
+    fn hash(&self) -> H256 {
+        let m = bincode::serialize(&self).unwrap();
+        digest::digest(&digest::SHA256, m.as_ref()).into()
+    }
+}
+   
 /// Create digital signature of a transaction
 pub fn sign(t: &Transaction, key: &Ed25519KeyPair) -> Signature {
-    unimplemented!()
+   // unimplemented!()
+   let message: Vec<u8> = bincode::serialize(&t).unwrap();
+   let hash_value: Vec<u8> = digest::digest(&digest::SHA256, &message).as_ref().to_vec();
+   key.sign(&hash_value)
 }
 
 /// Verify digital signature of a transaction, using public key instead of secret key
-pub fn verify(t: &Transaction, public_key: &<Ed25519KeyPair as KeyPair>::PublicKey, signature: &Signature) -> bool {
-    unimplemented!()
+pub fn verify(t: &Transaction, public_key: &[u8], signature: &[u8]) -> bool {
+    //unimplemented!()
+    //, public_key: &<Ed25519KeyPair as KeyPair>::PublicKey,
+    let message: Vec<u8> = bincode::serialize(&t).unwrap();
+    let hash_value: Vec<u8> = digest::digest(&digest::SHA256, &message).as_ref().to_vec();    
+    let pubic_key = signature::UnparsedPublicKey::new(&signature::ED25519, public_key);
+    pubic_key.verify(&hash_value, signature).is_ok()
 }
 
 #[cfg(any(test, test_utilities))]
@@ -21,8 +57,12 @@ mod tests {
     use crate::crypto::key_pair;
 
     pub fn generate_random_transaction() -> Transaction {
-        Default::default()
+       // Default::default()
         //unimplemented!()
+        let test_data_1 = vec![input_mode{message: generate_random_hash()}];
+        let test_data_2 = vec![output_mode{message: generate_random_hash(),}];
+
+        Transaction{input_data:test_data_1, output_data:test_data_2,};
     }
 
     #[test]
